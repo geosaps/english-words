a1c72c9ed2c93aadfc821a261e2e647c7bcf09a4
Pavlo Saps <emandsaps@gmail.com>
2017-10-03 19:32:06 +0300

    Add some styles

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import WordsList from './word/WordList';
 const App = (props) => {
   return (
     <div className="container">
-      <h1 className="text-center">English words to remember:</h1> 
+      <h1 className="text-center">English words:</h1> 
       <WordForm />
       <WordsList />
     </div>
